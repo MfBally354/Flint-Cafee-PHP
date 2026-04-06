81058c077e989e20ae26eef661d2d90973921156
MfBally354 <iqbalguntur313@gmail.com>
2026-04-06 17:39:10 +0700

    Auto commit

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 define('DB_HOST', 'db');
 define('DB_NAME', 'flintcafe');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', '#iqbaldebian#');
 define('DB_CHARSET', 'utf8mb4');
 
 define('CAFE_NAME', 'Flint Cafe');
